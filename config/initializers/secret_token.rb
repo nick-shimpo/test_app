e52b5c2e3cd40aafdbf66136457b284e03cdea25
nick <mr.nicholas.goodall@gmail.com>
2013-06-15 15:41:11 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shimpo::Application.config.secret_token = '20ec4de3536a1f7b132ef043bd93900f2a821139807251b6dcd79cd3eefa25b7bc476302c3d18e93bf79de5dc6418f68ee1753520524ba00239cc5f6b2cfbbc3'
